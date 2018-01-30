6ecfd7dbef065fbc93ac94b81fca7ce8a68f3750
严玉龙 <yulongyan1994@live.com>
2018-01-30 16:23:30 +0800

    数据库连接池和Spring Template 配置

--- src/cn/wkiki/mrc/config/CommonConfig.java
+++ src/cn/wkiki/mrc/config/CommonConfig.java
@@ -2,17 +2,22 @@ package cn.wkiki.mrc.config;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
 import cn.wkiki.mrc.protocol.SocketPool;
 import cn.wkiki.util.ConfigInfo;
 
 @Configuration
 @ImportResource(locations = { "classpath*:/cn/wkiki/mrc/config/CommonConfig.xml" })
-@ComponentScan(basePackages={"cn.wkiki.mrc.protocol"})
+@ComponentScan(basePackages={"cn.wkiki.mrc.protocol","cn.wkiki.mrc.protocol.impl","cn.wkiki.mrc.dao"})
 public class CommonConfig {
 	
 	@Bean("socketPool")
@@ -33,5 +38,33 @@ public class CommonConfig {
 		TimeUnit unit=TimeUnit.SECONDS;
 		java.util.concurrent.LinkedBlockingDeque<Runnable> workQueue=new java.util.concurrent.LinkedBlockingDeque<>();
 		return new java.util.concurrent.ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+	}
+	
+	@Bean("DBCP")
+	public BasicDataSource getDataSource(ConfigInfo configInfo)
+	{
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl(configInfo.getSqlConnectionStr());
+		dataSource.setUsername(configInfo.getSqlUserName());
+		dataSource.setPassword(configInfo.getSqlUserPwd());
+		dataSource.setInitialSize(5);
+		dataSource.setMaxActive(10);
+		return dataSource;
+	}
+
+	@Bean("jdbcTemplate")
+	@Autowired
+	public JdbcTemplate jdbcTemplate(javax.sql.DataSource source)
+
+	{
+		return new JdbcTemplate(source);
+	}
+	
+	@Bean("namedJdbcTemplate")
+	@Autowired
+	public NamedParameterJdbcTemplate namedJdbcTemplate(javax.sql.DataSource source)
+	{
+		return new NamedParameterJdbcTemplate(source);
 	}
 }
